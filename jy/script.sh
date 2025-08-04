210af16b54fcf0779007faec043799111d4649aa
ty77700 <ty636336@gmail.com>
2025-08-04 20:26:59 +0900

    feat: uv는 안된대

--- jy/script.sh
+++ jy/script.sh
@@ -4,7 +4,8 @@ python -m pip install --upgrade pip setuptools wheel
 pip install uv pip
 uv pip install --pre torchvision torchaudio torch --index-url https://download.pytorch.org/whl/nightly/xpu
 uv pip install opencv-python h5py tqdm
-uv pip install mmcv==2.1.0 mmdet mmengine
+uv pip install mmdet mmengine
+pip install mmcv==2.1.0
 uv pip install ultralytics
 python -m pip install -e .
 cd jy
